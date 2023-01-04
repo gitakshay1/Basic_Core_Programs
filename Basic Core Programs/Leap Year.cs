39c9dc6ddf04da1f854cafaeca983bb912ae543e
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-04 19:56:28 +0530

    [Akshay] Added Program of Leap Year

---
+++ Basic Core Programs/Leap Year.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    internal class Leap_Year
+    {
+        public void LeapYear()
+        {
+            Console.WriteLine("Enter the Year");
+            int Year = Convert.ToInt32(Console.ReadLine());
+            if((Year%4== 0)&&(Year%100!=0)||(Year%400==0) )
+            {
+                Console.WriteLine(Year + " is Leap Year");
+            }
+            else
+            {
+                Console.WriteLine(Year + " is Leap not Year");
+            }
+        }
+    }
+}
--- Basic Core Programs/Program.cs
+++ Basic Core Programs/Program.cs
@@ -4,13 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose the Program\n1 Flip Coin");
+            Console.WriteLine("Choose the Program\n1 Flip Coin\n2 Leap Year");
             int Choise=Convert.ToInt32(Console.ReadLine());
             switch(Choise)
             {
                 case 1:
                     Flip_Coin basic = new Flip_Coin();
                     basic.FlipCoin();
+                    break;
+                case 2:
+                    Leap_Year leap_Year=new Leap_Year();
+                    leap_Year.LeapYear();
                     break;
                 default:
                     Console.WriteLine("Enter Valid Choice");
