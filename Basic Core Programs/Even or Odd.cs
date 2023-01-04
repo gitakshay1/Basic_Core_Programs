6c53aa057b8a2ca474f92a8eb92d4609c3cb3dda
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-04 21:49:02 +0530

    [Akshay] Added Program to Check Even or odd

---
+++ Basic Core Programs/Even or Odd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    internal class Even_or_Odd
+    {
+        public void EvenOrOdd()
+        {
+            Console.WriteLine("Enter Number");
+            int Num=Convert.ToInt32(Console.ReadLine());
+            if(Num%2==0 )
+            {
+                Console.WriteLine(Num + " is Even Number");
+            }
+            else
+            {
+                Console.WriteLine(Num + " is Odd Number");
+            }
+        }
+    }
+}
--- Basic Core Programs/Program.cs
+++ Basic Core Programs/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Choose the Program\n1 Flip Coin\n2 Leap Year\n3 Power Of 2" +
-                "\n4 Harmonic Number\n5 Prime Factors\n6 Quotient and Reminder\n7 Swap Numbers");
+                "\n4 Harmonic Number\n5 Prime Factors\n6 Quotient and Reminder\n7 Swap Numbers" +
+                "\n8 Even Or Odd");
             int Choise=Convert.ToInt32(Console.ReadLine());
             switch(Choise)
             {
@@ -36,6 +37,10 @@
                 case 7:
                     Swap_Numbers swap_Numbers=new Swap_Numbers();
                     swap_Numbers.Swap_Number();
+                    break;
+                case 8:
+                    Even_or_Odd even_Or_Odd=new Even_or_Odd();
+                    even_Or_Odd.EvenOrOdd();
                     break;
                 default:
                     Console.WriteLine("Enter Valid Choice");
