a1c2236067ec810ffc1ba7b054e5438f40205dd0
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-04 21:39:22 +0530

    [Akshay] Added program to SWap two numbers

--- Basic Core Programs/Program.cs
+++ Basic Core Programs/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Choose the Program\n1 Flip Coin\n2 Leap Year\n3 Power Of 2" +
-                "\n4 Harmonic Number\n5 Prime Factors\n6 Quotient and Reminder");
+                "\n4 Harmonic Number\n5 Prime Factors\n6 Quotient and Reminder\n7 Swap Numbers");
             int Choise=Convert.ToInt32(Console.ReadLine());
             switch(Choise)
             {
@@ -32,6 +32,10 @@
                 case 6:
                     Quotient_and_Reminder compute=new Quotient_and_Reminder();
                     compute.compute_Quot_Remin();
+                    break;
+                case 7:
+                    Swap_Numbers swap_Numbers=new Swap_Numbers();
+                    swap_Numbers.Swap_Number();
                     break;
                 default:
                     Console.WriteLine("Enter Valid Choice");
---
+++ Basic Core Programs/Swap Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    internal class Swap_Numbers
+    {
+        public void Swap_Number()
+        {
+            Console.WriteLine("Enter First Number");    
+            int Num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            int Num2=Convert.ToInt32(Console.ReadLine());
+            int temp;
+            temp = Num1;
+            Num1 = Num2;
+            Num2 = temp;
+            Console.WriteLine("After Swaping First Number is "+Num1+" Second Number "+Num2);
+
+        }
+    }
+}
