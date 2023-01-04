5b78e240ef44308db691d08f0473a2f5a4b2192c
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-04 20:36:48 +0530

    [Akshay] Added Program for Power of 2 table

---
+++ Basic Core Programs/Power of 2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    internal class Power_of_2
+    {
+        public void Pow_of_2()
+        {
+            Console.WriteLine("Enter Power Value N");
+            int N=Convert.ToInt32(Console.ReadLine());
+            if((0<=N) &&(N<31))
+            {
+                for(int n=0;n<=N;n++)
+                {
+                    Console.WriteLine(Math.Pow(2,n));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter power between 0 to 30");
+            }
+        }
+    }
+}
--- Basic Core Programs/Program.cs
+++ Basic Core Programs/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose the Program\n1 Flip Coin\n2 Leap Year");
+            Console.WriteLine("Choose the Program\n1 Flip Coin\n2 Leap Year\n3 Power Of 2");
             int Choise=Convert.ToInt32(Console.ReadLine());
             switch(Choise)
             {
@@ -15,6 +15,10 @@
                 case 2:
                     Leap_Year leap_Year=new Leap_Year();
                     leap_Year.LeapYear();
+                    break;
+                case 3:
+                    Power_of_2 power_Of_2=new Power_of_2();
+                    power_Of_2.Pow_of_2();
                     break;
                 default:
                     Console.WriteLine("Enter Valid Choice");
