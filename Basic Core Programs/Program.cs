e1e5828c81f939df3013d2634a1d54734bf5573a
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-04 21:25:27 +0530

    [Akshay] Added Program to compute Quotient and reminder

--- Basic Core Programs/Program.cs
+++ Basic Core Programs/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Choose the Program\n1 Flip Coin\n2 Leap Year\n3 Power Of 2" +
-                "\n4 Harmonic Number\n5 Prime Factors");
+                "\n4 Harmonic Number\n5 Prime Factors\n6 Quotient and Reminder");
             int Choise=Convert.ToInt32(Console.ReadLine());
             switch(Choise)
             {
@@ -28,6 +28,10 @@
                 case 5:
                     Factors factors=new Factors();
                     factors.Prime_Factors();
+                    break;
+                case 6:
+                    Quotient_and_Reminder compute=new Quotient_and_Reminder();
+                    compute.compute_Quot_Remin();
                     break;
                 default:
                     Console.WriteLine("Enter Valid Choice");
---
+++ Basic Core Programs/Quotient and Reminder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    internal class Quotient_and_Reminder
+    {
+        public void compute_Quot_Remin()
+        {
+            Console.WriteLine("Enter Number");
+            int Num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Divider");
+            int Divider=Convert.ToInt32(Console.ReadLine());
+            int Quotient, Reminder;
+            Quotient = Num / Divider;
+            Reminder=Num% Divider;
+            Console.WriteLine("Quotient is " + Quotient + " Reminder is " + Reminder);
+        }
+    }
+}
